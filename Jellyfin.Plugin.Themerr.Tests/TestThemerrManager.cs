b4c683e5b6a427e68928708e2e3fe7cb4ac13858
ReenigneArcher <42013603+ReenigneArcher@users.noreply.github.com>
2023-12-11 23:18:25 -0500

    tests: improve test coverage (#101)

--- Jellyfin.Plugin.Themerr.Tests/TestThemerrManager.cs
+++ Jellyfin.Plugin.Themerr.Tests/TestThemerrManager.cs
@@ -1,17 +1,59 @@
-using Xunit.Abstractions;
+using MediaBrowser.Controller.Library;
+using MetadataProvider = MediaBrowser.Model.Entities.MetadataProvider;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Newtonsoft.Json;
 
 namespace Jellyfin.Plugin.Themerr.Tests;
 
+[Collection("Bootstrapped Collection")]
 public class TestThemerrManager
 {
-    private readonly ITestOutputHelper _testOutputHelper;
+    private readonly ThemerrManager _themerrManager;
 
-    public TestThemerrManager(ITestOutputHelper testOutputHelper)
+    public TestThemerrManager(ITestOutputHelper output)
     {
-        _testOutputHelper = testOutputHelper;
+        TestLogger.Initialize(output);
+        
+        Mock<ILibraryManager> mockLibraryManager = new();
+        Mock<ILogger<ThemerrManager>> mockLogger = new();
+        
+        _themerrManager = new ThemerrManager(mockLibraryManager.Object, mockLogger.Object);
     }
-
+    
+    private static List<string> FixtureYoutubeUrls()
+    {
+        // create a list and return it
+        var youtubeUrls = new List<string>()
+        {
+            "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+            "https://www.youtube.com/watch?v=yPYZpwSpKmA",
+            "https://www.youtube.com/watch?v=Ghmd4QzT9YY",
+            "https://www.youtube.com/watch?v=LVEWkghDh9A"
+        };
+        
+        // return the list
+        return youtubeUrls;
+    }
+    
+    private List<string> FixtureThemerrDbUrls()
+    {
+        // make list of youtubeUrls to populate
+        var youtubeUrls = new List<string>();
+        
+        foreach (var movie in BootstrapJellyfinServer.MockMovies())
+        {
+            var tmdbId = movie.ProviderIds[MetadataProvider.Tmdb.ToString()];
+            var themerrDbLink = _themerrManager.CreateThemerrDbLink(tmdbId);
+            youtubeUrls.Add(themerrDbLink);
+        }
+        
+        // return the list
+        return youtubeUrls;
+    }
+    
     [Fact]
+    [Trait("Category", "Unit")]
     public void TestSaveMp3()
     {
         // set destination with themerr_jellyfin_tests as the folder name
@@ -20,50 +62,41 @@ public class TestThemerrManager
             "theme.mp3"
             );
         
-        // create a list of youtube urls
-        var videoUrls = new List<string>
-        {
-            "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-            "https://www.youtube.com/watch?v=yPYZpwSpKmA",
-            "https://www.youtube.com/watch?v=Ghmd4QzT9YY",
-            "https://www.youtube.com/watch?v=LVEWkghDh9A"
-        };
-        foreach (var videoUrl in videoUrls)
+        
+        foreach (var videoUrl in FixtureYoutubeUrls())
         {
             // log
-            _testOutputHelper.WriteLine($"Attempting to download {videoUrl}");
+            TestLogger.Info($"Attempting to download {videoUrl}");
             
             // run and wait
-            ThemerrManager.SaveMp3(destinationFile, videoUrl);
-            
-            // wait until the file is downloaded
-            Thread.Sleep(5000); // 5 seconds
+            var themeExists = _themerrManager.SaveMp3(destinationFile, videoUrl);
+            Assert.True(themeExists, $"SaveMp3 did not return True for {videoUrl}");
             
             // check if file exists
-            Assert.True(File.Exists(destinationFile));
+            Assert.True(File.Exists(destinationFile), $"File {destinationFile} does not exist");
             
             // check if the file is an actual mp3
             // https://en.wikipedia.org/wiki/List_of_file_signatures
             var fileBytes = File.ReadAllBytes(destinationFile);
             var fileBytesHex = BitConverter.ToString(fileBytes);
             
-            // make sure the file does is not WebM, starts with `1A 45 DF A3`
-            var isNotWebM = !fileBytesHex.StartsWith("1A-45-DF-A3");
-            Assert.True(isNotWebM);
+            // make sure the file is not WebM, starts with `1A 45 DF A3`
+            var isWebM = fileBytesHex.StartsWith("1A-45-DF-A3");
+            Assert.False(isWebM, $"File {destinationFile} is WebM");
             
             // valid mp3 signatures dictionary with offsets
             var validMp3Signatures = new Dictionary<string, int>
             {
-                {"66-74-79-70-64-61-73-68", 4}, // Mp4 container?
-                {"66-74-79-70-69-73-6F-6D", 4}, // Mp4 container
-                {"49-44-33", 0}, // ID3
-                {"FF-FB", 0}, // MPEG-1 Layer 3
-                {"FF-F3", 0}, // MPEG-1 Layer 3
-                {"FF-F2", 0} // MPEG-1 Layer 3
+                {"66-74-79-70-64-61-73-68", 4},  // Mp4 container?
+                {"66-74-79-70-69-73-6F-6D", 4},  // Mp4 container
+                {"49-44-33", 0},  // ID3
+                {"FF-FB", 0},  // MPEG-1 Layer 3
+                {"FF-F3", 0},  // MPEG-1 Layer 3
+                {"FF-F2", 0}  // MPEG-1 Layer 3
             };
             
             // log beginning of fileBytesHex
-            _testOutputHelper.WriteLine($"Beginning of fileBytesHex: {fileBytesHex.Substring(0, 40)}");
+            TestLogger.Debug($"Beginning of fileBytesHex: {fileBytesHex.Substring(0, 40)}");
             
             // check if the file is an actual mp3
             var isMp3 = false;
@@ -72,7 +105,7 @@ public class TestThemerrManager
             foreach (var (signature, offset) in validMp3Signatures)
             {
                 // log
-                _testOutputHelper.WriteLine($"Checking for {signature} at offset of {offset} bytes");
+                TestLogger.Debug($"Checking for {signature} at offset of {offset} bytes");
                 
                 // remove the offset bytes
                 var fileBytesHexWithoutOffset = fileBytesHex.Substring(offset * 3);
@@ -82,7 +115,7 @@ public class TestThemerrManager
                 if (isSignature)
                 {
                     // log
-                    _testOutputHelper.WriteLine($"Found {signature} at offset {offset}");
+                    TestLogger.Info($"Found {signature} at offset {offset}");
                 
                     // set isMp3 to true
                     isMp3 = true;
@@ -92,12 +125,172 @@ public class TestThemerrManager
                 }
 
                 // log
-                _testOutputHelper.WriteLine($"Did not find {signature} at offset {offset}");
+                TestLogger.Debug($"Did not find {signature} at offset {offset}");
             }
-            Assert.True(isMp3);
+            Assert.True(isMp3, $"File {destinationFile} is not an mp3");
             
             // delete file
             File.Delete(destinationFile);
+        }
+    }
+    
+    [Fact]
+    [Trait("Category", "Unit")]
+    public void TestSaveMp3InvalidUrl()
+    {
+        // set destination with themerr_jellyfin_tests as the folder name
+        var destinationFile = Path.Combine(
+            // "themerr_jellyfin_tests",
+            "theme.mp3"
+            );
+        
+        // set invalid url
+        var invalidUrl = "https://www.youtube.com/watch?v=invalid";
+        
+        // run and wait
+        var themeExists = _themerrManager.SaveMp3(destinationFile, invalidUrl);
+        Assert.False(themeExists, $"SaveMp3 did not return False for {invalidUrl}");
+        
+        // check if file exists
+        Assert.False(File.Exists(destinationFile), $"File {destinationFile} exists");
+    }
+
+    // todo: fix this test
+    // [Fact]
+    // [Trait("Category", "Unit")]
+    // public void TestProcessMovieTheme()
+    // {
+    //     // get bootstrapped movies
+    //     var mockMovies = BootstrapJellyfinServer.MockMovies();
+    //     
+    //     Assert.True(mockMovies.Count > 0, "mockMovies.Count is not greater than 0");
+    //     
+    //     foreach (var movie in mockMovies)
+    //     {
+    //         // get the movie theme
+    //         _themerrManager.ProcessMovieTheme(movie);
+    //         
+    //         Assert.True(File.Exists(_themerrManager.GetThemePath(movie)), $"File {_themerrManager.GetThemePath(movie)} does not exist");
+    //     }
+    // }
+
+    [Fact]
+    [Trait("Category", "Unit")]
+    public void TestShouldSkipDownload()
+    {
+        var themePath = Path.Combine(
+            "theme.mp3"
+            );
+        var themerrDataPath = Path.Combine(
+            "themerr_data.json"
+            );
+        
+        var shouldSkipDownload = _themerrManager.ShouldSkipDownload(themePath, themerrDataPath);
+        Assert.False(shouldSkipDownload, "ShouldSkipDownload returned True");
+    }
+
+    [Fact]
+    [Trait("Category", "Unit")]
+    public void TestGetThemePath()
+    {
+        // get bootstrapped movies
+        var mockMovies = BootstrapJellyfinServer.MockMovies();
+        
+        Assert.True(mockMovies.Count > 0, "mockMovies.Count is not greater than 0");
+        
+        foreach (var movie in mockMovies)
+        {
+            // get the movie theme
+            var themePath = _themerrManager.GetThemePath(movie);
+            
+            // ensure path ends with theme.mp3
+            Assert.EndsWith("theme.mp3", themePath);
+        }
+    }
+    
+    [Fact]
+    [Trait("Category", "Unit")]
+    public void TestGetThemerrDataPath()
+    {
+        // get bootstrapped movies
+        var mockMovies = BootstrapJellyfinServer.MockMovies();
+        
+        Assert.True(mockMovies.Count > 0, "mockMovies.Count is not greater than 0");
+        
+        foreach (var movie in mockMovies)
+        {
+            // get the movie theme
+            var themerrDataPath = _themerrManager.GetThemerrDataPath(movie);
+            
+            // ensure path ends with theme.mp3
+            Assert.EndsWith("themerr.json", themerrDataPath);
+        }
+    }
+
+    [Fact]
+    [Trait("Category", "Unit")]
+    public void TestCreateThemerrDbLink()
+    {
+        // get bootstrapped movies
+        var mockMovies = BootstrapJellyfinServer.MockMovies();
+        
+        Assert.True(mockMovies.Count > 0, "mockMovies.Count is not greater than 0");
+
+        foreach (var movie in mockMovies)
+        {
+            var tmdbId = movie.ProviderIds[MetadataProvider.Tmdb.ToString()];
+            var themerrDbUrl = _themerrManager.CreateThemerrDbLink(tmdbId);
+            
+            TestLogger.Info($"themerrDbLink: {themerrDbUrl}");
+
+            Assert.EndsWith($"themoviedb/{tmdbId}.json", themerrDbUrl);
+        }
+    }
+
+    [Fact]
+    [Trait("Category", "Unit")]
+    public void TestGetYoutubeThemeUrl()
+    {
+        
+        // loop over each themerrDbLink
+        foreach (var themerrDbLink in FixtureThemerrDbUrls())
+        {
+            // get the new youtube theme url
+            var youtubeThemeUrl = _themerrManager.GetYoutubeThemeUrl(themerrDbLink, $"test{themerrDbLink}");
+
+            // log
+            TestLogger.Info($"youtubeThemeUrl: {youtubeThemeUrl}");
+
+            Assert.NotEmpty(youtubeThemeUrl);
+        }
+    }
+
+    [Fact]
+    [Trait("Category", "Unit")]
+    public void TestSaveThemerrData()
+    {
+        
+        // set mock themerrDataPath using a random number
+        var mockThemerrDataPath = $"themerr_{new Random().Next()}.json";
+        
+        // loop over each themerrDbLink
+        foreach (var youtubeThemeUrl in FixtureYoutubeUrls())
+        {
+            
+            // save themerr data
+            var fileExists = _themerrManager.SaveThemerrData(mockThemerrDataPath, youtubeThemeUrl);
+            Assert.True(fileExists, $"SaveThemerrData did not return True for {youtubeThemeUrl}");
+            
+            // check if file exists
+            Assert.True(File.Exists(mockThemerrDataPath), $"File {mockThemerrDataPath} does not exist");
+            
+            // make sure the saved json file contains a key named "youtube_theme_url", and value is correct
+            var jsonString = File.ReadAllText(mockThemerrDataPath);
+            File.Delete(mockThemerrDataPath);  // delete the file
+            dynamic jsonData = JsonConvert.DeserializeObject(jsonString) ?? throw new InvalidOperationException();
+            var savedYoutubeThemeUrl = jsonData.youtube_theme_url.ToString();
+            Assert.True(youtubeThemeUrl == savedYoutubeThemeUrl,
+                $"youtubeThemeUrl {youtubeThemeUrl} does not match savedYoutubeThemeUrl {savedYoutubeThemeUrl}");
         }
     }
 }
--- Jellyfin.Plugin.Themerr/ThemerrManager.cs
+++ Jellyfin.Plugin.Themerr/ThemerrManager.cs
@@ -20,40 +20,65 @@ using YoutubeExplode.Videos.Streams;
 namespace Jellyfin.Plugin.Themerr
 
 {
+    /// <summary>
+    /// The main entry point for the plugin.
+    /// </summary>
     public class ThemerrManager : IServerEntryPoint
     {
         private readonly ILibraryManager _libraryManager;
         private readonly Timer _timer;
         private readonly ILogger<ThemerrManager> _logger;
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="libraryManager"></param>
+        /// <param name="logger"></param>
         public ThemerrManager(ILibraryManager libraryManager, ILogger<ThemerrManager> logger)
         {
             _libraryManager = libraryManager;
             _logger = logger;
             _timer = new Timer(_ => OnTimerElapsed(), null, Timeout.Infinite, Timeout.Infinite);
         }
-
         
-        public static void SaveMp3(string destination, string videoUrl)
+        /// <summary>
+        /// Save a mp3 file from a youtube video url.
+        /// </summary>
+        /// <param name="destination"></param>
+        /// <param name="videoUrl"></param>
+        public bool SaveMp3(string destination, string videoUrl)
         {
-            Task.Run(async () =>
+            try
             {
-                var youtube = new YoutubeClient();
-                var streamManifest = await youtube.Videos.Streams.GetManifestAsync(videoUrl);
+                Task.Run(async () =>
+                {
+                    var youtube = new YoutubeClient();
+                    var streamManifest = await youtube.Videos.Streams.GetManifestAsync(videoUrl);
 
-                // highest bitrate audio mp3 stream
-                var streamInfo = streamManifest
-                    .GetAudioOnlyStreams()
-                    .Where(s => s.Container == Container.Mp4)
-                    .GetWithHighestBitrate();
+                    // highest bitrate audio mp3 stream
+                    var streamInfo = streamManifest
+                        .GetAudioOnlyStreams()
+                        .Where(s => s.Container == Container.Mp4)
+                        .GetWithHighestBitrate();
 
-                // Download the stream to a file
-                await youtube.Videos.Streams.DownloadAsync(streamInfo, destination);
-            });
+                    // Download the stream to a file
+                    await youtube.Videos.Streams.DownloadAsync(streamInfo, destination);
+                });
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("Unable to download {VideoUrl} to {Destination}: {Error}", videoUrl, destination, e);
+                return false;
+            }
+
+            return WaitForFile(destination, 30000);
         }
         
-        
-        private IEnumerable<Movie> GetMoviesFromLibrary()
+        /// <summary>
+        /// Get all movies from the library that have a tmdb id.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Movie> GetMoviesFromLibrary()
         {
             return _libraryManager.GetItemList(new InternalItemsQuery
             {
@@ -63,115 +88,225 @@ namespace Jellyfin.Plugin.Themerr
                 HasTmdbId = true
             }).Select(m => m as Movie);
         }
-
-
+        
+        /// <summary>
+        /// Enumerate through all movies in the library and downloads their theme songs as required.
+        /// </summary>
+        /// <returns></returns>
         public Task DownloadAllThemerr()
         {
             var movies = GetMoviesFromLibrary();
             foreach (var movie in movies)
             {
-                // set paths
-                var themePath = $"{movie.ContainingFolderPath}/theme.mp3";
-                var themerrDataPath = $"{movie.ContainingFolderPath}/themerr.json";
-                
-                // if theme.mp3 exists and themerr.json does not exist then skip
-                // don't overwrite user supplied theme files
-                if (System.IO.File.Exists(themePath) && !System.IO.File.Exists(themerrDataPath))
-                {
-                    continue;
-                }
-                
-                // open themerr.json and check if theme song is already downloaded
-                var existingYoutubeThemeUrl = "";
-                if (System.IO.File.Exists(themerrDataPath))
-                {
-                    var jsonString = System.IO.File.ReadAllText(themerrDataPath);
-                    dynamic jsonData = JsonConvert.DeserializeObject(jsonString);
-                    if (jsonData != null)
-                    {
-                        existingYoutubeThemeUrl = jsonData.youtube_theme_url;
-                    }
-                }
-                
-                // get tmdb id
-                var tmdb = movie.GetProviderId(MetadataProvider.Tmdb);
-                // create themerrdb_link
-                var themerrDbLink = $"https://app.lizardbyte.dev/ThemerrDB/movies/themoviedb/{tmdb}.json";
-
-                // download themerrdb_link as a json object
-                var client = new HttpClient();
-                try
-                {
-                    var jsonString = client.GetStringAsync(themerrDbLink).Result;
-                    // serialize the json object
-                    dynamic jsonData = JsonConvert.DeserializeObject(jsonString);
-                    if (jsonData != null)
-                    {
-                        // extract the youtube_theme_url key (string)
-                        string youtubeThemeUrl = jsonData.youtube_theme_url;
-                        
-                        // if youtubeThemeUrl is not equal to existingYoutubeThemeUrl then download
-                        if (youtubeThemeUrl == existingYoutubeThemeUrl)
-                        {
-                            continue;
-                        }
-
-                        _logger.LogDebug("Trying to download {movieName}, {youtubeThemeUrl}",
-                            movie.Name, youtubeThemeUrl);
-                        
-                        try
-                        {
-                            SaveMp3(themePath, youtubeThemeUrl);
-                            _logger.LogInformation("{movieName} theme song successfully downloaded",
-                                movie.Name);
-                            // create themerr.json (json object) with these keys, youtube_theme_url, downloaded_timestamp
-                            var themerrData = new
-                            {
-                                downloaded_timestamp = DateTime.UtcNow,
-                                youtube_theme_url = youtubeThemeUrl
-                            };
-                            // write themerr.json to disk
-                            System.IO.File.WriteAllText(themerrDataPath, JsonConvert.SerializeObject(themerrData));
-                            
-                            // update the metadata
-                            movie.RefreshMetadata(CancellationToken.None);
-                        }
-                        catch (Exception e)
-                        {
-                            _logger.LogError("Unable to download {movieName} theme song: {error}",
-                                movie.Name, e);
-                        }
-                    }
-                    else
-                    {
-                        _logger.LogInformation("{movieName} theme song not in database, or no internet connection",
-                            movie.Name);
-                    }
-                    
-                }
-                catch (Exception)
-                {
-                    _logger.LogInformation("{movieName} theme song not in database, or no internet connection",
-                        movie.Name);
-                }
+                ProcessMovieTheme(movie);
             }
+
             return Task.CompletedTask;
         }
 
+        /// <summary>
+        /// Download the theme song for a movie if it doesn't already exist.
+        /// </summary>
+        /// <param name="movie"></param>
+        public void ProcessMovieTheme(Movie movie)
+        {
+            var movieTitle = movie.Name;
+            var themePath = GetThemePath(movie);
+            var themerrDataPath = GetThemerrDataPath(movie);
 
+            if (ShouldSkipDownload(themePath, themerrDataPath))
+            {
+                return;
+            }
+
+            var existingYoutubeThemeUrl = GetExistingYoutubeThemeUrl(themerrDataPath);
+
+            // get tmdb id
+            var tmdbId = movie.GetProviderId(MetadataProvider.Tmdb);
+            // create themerrdb url
+            var themerrDbLink = CreateThemerrDbLink(tmdbId);
+
+            var youtubeThemeUrl = GetYoutubeThemeUrl(themerrDbLink, movieTitle);
+
+            if (string.IsNullOrEmpty(youtubeThemeUrl) || youtubeThemeUrl == existingYoutubeThemeUrl)
+            {
+                return;
+            }
+            
+            SaveMp3(themePath, youtubeThemeUrl);
+            SaveThemerrData(themerrDataPath, youtubeThemeUrl);
+            movie.RefreshMetadata(CancellationToken.None);
+        }
+
+        /// <summary>
+        /// Check if the theme song should be downloaded.
+        ///
+        /// If theme.mp3 exists and themerr.json doesn't exist, then skip to avoid overwriting user supplied themes.
+        /// </summary>
+        /// <param name="themePath"></param>
+        /// <param name="themerrDataPath"></param>
+        /// <returns></returns>
+        public bool ShouldSkipDownload(string themePath, string themerrDataPath)
+        {
+            return System.IO.File.Exists(themePath) && !System.IO.File.Exists(themerrDataPath);
+        }
+
+        /// <summary>
+        /// Get the path to the theme song.
+        /// </summary>
+        /// <param name="movie"></param>
+        /// <returns></returns>
+        public string GetThemePath(Movie movie)
+        {
+            return $"{movie.ContainingFolderPath}/theme.mp3";
+        }
+
+        /// <summary>
+        /// Get the path to the themerr data file.
+        /// </summary>
+        /// <param name="movie"></param>
+        /// <returns></returns>
+        public string GetThemerrDataPath(Movie movie)
+        {
+            return $"{movie.ContainingFolderPath}/themerr.json";
+        }
+
+        /// <summary>
+        /// Get the existing youtube theme url from the themerr data file if it exists.
+        /// </summary>
+        /// <param name="themerrDataPath"></param>
+        /// <returns></returns>
+        public static string GetExistingYoutubeThemeUrl(string themerrDataPath)
+        {
+            if (!System.IO.File.Exists(themerrDataPath))
+                return string.Empty;
+            
+            var jsonString = System.IO.File.ReadAllText(themerrDataPath);
+            dynamic jsonData = JsonConvert.DeserializeObject(jsonString);
+            return jsonData?.youtube_theme_url;
+        }
+
+        /// <summary>
+        /// Create a link to the themerr database.
+        /// </summary>
+        /// <param name="tmdbId"></param>
+        /// <returns></returns>
+        public string CreateThemerrDbLink(string tmdbId)
+        {
+            return $"https://app.lizardbyte.dev/ThemerrDB/movies/themoviedb/{tmdbId}.json";
+        }
+
+        /// <summary>
+        /// Get the YouTube theme url from the themerr database.
+        /// </summary>
+        /// <param name="themerrDbUrl"></param>
+        /// <param name="movieTitle"></param>
+        /// <returns></returns>
+        public string GetYoutubeThemeUrl(string themerrDbUrl, string movieTitle)
+        {
+            var client = new HttpClient();
+
+            try
+            {
+                var jsonString = client.GetStringAsync(themerrDbUrl).Result;
+                dynamic jsonData = JsonConvert.DeserializeObject(jsonString);
+                return jsonData?.youtube_theme_url;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("Unable to get theme song for {MovieTitle}: {Error}", movieTitle, e);
+                return string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// Save the themerr data file.
+        /// </summary>
+        /// <param name="themerrDataPath"></param>
+        /// <param name="youtubeThemeUrl"></param>
+        /// <returns></returns>
+        public bool SaveThemerrData(string themerrDataPath, string youtubeThemeUrl)
+        {
+            var success = false;
+            var themerrData = new
+            {
+                downloaded_timestamp = DateTime.UtcNow,
+                youtube_theme_url = youtubeThemeUrl
+            };
+            try
+            {
+                System.IO.File.WriteAllText(themerrDataPath, JsonConvert.SerializeObject(themerrData));
+                success = true;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("Unable to save themerr data to {ThemerrDataPath}: {Error}", themerrDataPath, e);
+            }
+
+            return success && WaitForFile(themerrDataPath, 10000);
+        }
+        
+        /// <summary>
+        /// Wait for file to exist on disk.
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="timeout"></param>
+        /// <returns></returns>
+        public bool WaitForFile(string filePath, int timeout)
+        {
+            var startTime = DateTime.UtcNow;
+            while (!System.IO.File.Exists(filePath))
+            {
+                if (DateTime.UtcNow - startTime > TimeSpan.FromMilliseconds(timeout))
+                {
+                    return false;
+                }
+                Thread.Sleep(100);
+            }
+            
+            // wait until file is not being used by another process
+            var fileIsLocked = true;
+            while (fileIsLocked)
+            {
+                try
+                {
+                    using (System.IO.File.Open(filePath, System.IO.FileMode.Open)) { }
+                    fileIsLocked = false;
+                }
+                catch (System.IO.IOException)
+                {
+                    if (DateTime.UtcNow - startTime > TimeSpan.FromMilliseconds(timeout))
+                    {
+                        return false;
+                    }
+                    Thread.Sleep(100);
+                }
+            }
+            
+            return true;
+        }
+        
+        /// <summary>
+        /// Called when the plugin is loaded.
+        /// </summary>
         private void OnTimerElapsed()
         {
             // Stop the timer until next update
             _timer.Change(Timeout.Infinite, Timeout.Infinite);
         }
-
         
+        /// <summary>
+        /// Todo
+        /// </summary>
+        /// <returns></returns>
         public Task RunAsync()
         {
             return Task.CompletedTask;
         }
-
         
+        /// <summary>
+        /// Todo
+        /// </summary>
         public void Dispose()
         {
         }
