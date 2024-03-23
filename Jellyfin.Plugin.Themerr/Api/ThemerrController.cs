99655422f9a22e3f11f53023e6ca42ceaa7483fb
ReenigneArcher <42013603+ReenigneArcher@users.noreply.github.com>
2024-03-23 19:14:42 -0400

    feat(i18n): add localization support (#256)

--- Jellyfin.Plugin.Themerr/Api/ThemerrController.cs
+++ Jellyfin.Plugin.Themerr/Api/ThemerrController.cs
@@ -1,8 +1,12 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Mime;
+using System.Reflection;
 using System.Threading.Tasks;
+using MediaBrowser.Controller.Configuration;
 using MediaBrowser.Controller.Library;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -24,18 +28,22 @@ namespace Jellyfin.Plugin.Themerr.Api
     {
         private readonly ThemerrManager _themerrManager;
         private readonly ILogger<ThemerrManager> _logger;
+        private readonly IServerConfigurationManager _configurationManager;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="ThemerrController"/> class.
         /// </summary>
         /// <param name="libraryManager">The library manager.</param>
         /// <param name="logger">The logger.</param>
+        /// <param name="configurationManager">The configuration manager.</param>
         public ThemerrController(
             ILibraryManager libraryManager,
-            ILogger<ThemerrManager> logger)
+            ILogger<ThemerrManager> logger,
+            IServerConfigurationManager configurationManager)
         {
             _themerrManager = new ThemerrManager(libraryManager,  logger);
             _logger = logger;
+            _configurationManager = configurationManager;
         }
 
         /// <summary>
@@ -119,6 +127,112 @@ namespace Jellyfin.Plugin.Themerr.Api
             _logger.LogInformation("Progress Items: {Items}", JsonConvert.SerializeObject(tmpObject));
 
             return new JsonResult(tmpObject);
+        }
+
+        /// <summary>
+        /// Get the localization strings from Locale/{selected_locale}.json.
+        /// </summary>
+        ///
+        /// <returns>JSON object containing localization strings.</returns>
+        [HttpGet("GetTranslations")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult GetTranslations()
+        {
+            // get the locale from the user's settings
+            var culture = _configurationManager.Configuration.UICulture;
+
+            _logger.LogInformation("Server culture: {ServerCulture}", culture);
+
+            // get file paths from LocalizationManager
+            var filePaths = GetCultureResource(culture);
+
+            // Get the current assembly
+            var assembly = Assembly.GetExecutingAssembly();
+
+            for (var i = 0; i < filePaths.Count; i++)
+            {
+                // construct the resource name
+                var resourceName = $"Jellyfin.Plugin.Themerr.Locale.{filePaths[i]}";
+
+                // Get the resource stream
+                using var stream = assembly.GetManifestResourceStream(resourceName);
+
+                if (stream == null)
+                {
+                    _logger.LogWarning(
+                        "Locale resource does not exist: {ResourceName}",
+                        resourceName.Replace(Environment.NewLine, string.Empty));
+                    continue;
+                }
+
+                // Initialize the result dictionary
+                var result = new Dictionary<string, object>();
+
+                // read the resource content
+                using var reader = new StreamReader(stream);
+                var json = reader.ReadToEnd();
+
+                // deserialize the JSON content into a dictionary
+                var localizedStrings = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+
+                // Add the localized strings to the 'locale' key
+                result["locale"] = localizedStrings;
+
+                // Now get the fallback resource
+                var fallbackResourceName = "Jellyfin.Plugin.Themerr.Locale.en.json";
+                using var fallbackStream = assembly.GetManifestResourceStream(fallbackResourceName);
+
+                if (fallbackStream != null)
+                {
+                    // read the fallback resource content
+                    using var fallbackReader = new StreamReader(fallbackStream);
+                    var fallbackJson = fallbackReader.ReadToEnd();
+
+                    // deserialize the fallback JSON content into a dictionary
+                    var fallbackLocalizedStrings =
+                        JsonConvert.DeserializeObject<Dictionary<string, string>>(fallbackJson);
+
+                    // Add the fallback localized strings to the 'fallback' key
+                    result["fallback"] = fallbackLocalizedStrings;
+                }
+                else
+                {
+                    _logger.LogError("Fallback locale resource does not exist: {ResourceName}", fallbackResourceName);
+                }
+
+                // return the result as a JSON object
+                return Ok(result);
+            }
+
+            // return an error if we get this far
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+
+        /// <summary>
+        /// Get the resources of the given culture.
+        /// </summary>
+        ///
+        /// <param name="culture">The culture to get the resource for.</param>
+        /// <returns>A list of file names.</returns>
+        public List<string> GetCultureResource(string culture)
+        {
+            string tmp;
+            var fileNames = new List<string>();
+            var parts = culture.Split('-');
+
+            if (parts.Length == 2)
+            {
+                tmp = parts[0].ToLowerInvariant() + "_" + parts[1].ToUpperInvariant();
+                fileNames.Add(tmp + ".json");
+            }
+
+            tmp = parts[0].ToLowerInvariant();
+            if (tmp != "en")
+            {
+                fileNames.Add(tmp + ".json");
+            }
+
+            return fileNames;
         }
     }
 }
