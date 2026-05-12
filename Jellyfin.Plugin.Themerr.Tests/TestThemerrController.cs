LizardByte/Themerr-jellyfin

commit b9c734e200fdf18b67c5708b0e30acaa4a70a649

feat: add update interval config option (#189)

Jellyfin.Plugin.Themerr/Api/ThemerrController.cs
@@ -35,17 +35,17 @@ namespace Jellyfin.Plugin.Themerr.Api
         }
 
         /// <summary>
-        /// Downloads all Movie theme songs.
+        /// Trigger an update from the configuration html page.
         ///
         /// A response code of 204 indicates that the download has started successfully.
         /// </summary>
         /// <returns>A <see cref="NoContentResult"/> indicating success.</returns>
-        [HttpPost("DownloadMovies")]
+        [HttpPost("TriggerUpdate")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
-        public async Task DownloadMovieThemerrRequest()
+        public async Task TriggerUpdateRequest()
         {
-            _logger.LogInformation("Downloading Movie Theme Songs");
-            await _themerrManager.DownloadAllThemerr();
+            _logger.LogInformation("Updating Movie Theme Songs");
+            await _themerrManager.UpdateAll();
             _logger.LogInformation("Completed");
         }
     }

Jellyfin.Plugin.Themerr/ScheduledTasks/ThemerrTasks.cs
@@ -30,12 +30,12 @@ namespace Jellyfin.Plugin.Themerr.ScheduledTasks
         /// <summary>
         /// Gets the name of the task.
         /// </summary>
-        public string Name => "Download Theme Songs";
+        public string Name => "Update Theme Songs";
 
         /// <summary>
         /// Gets the key of the task.
         /// </summary>
-        public string Key => "Download ThemeSongs";
+        public string Key => "Update ThemeSongs";
 
         /// <summary>
         /// Gets the description of the task.
@@ -56,7 +56,7 @@ namespace Jellyfin.Plugin.Themerr.ScheduledTasks
         public async Task ExecuteAsync(IProgress<double> progress, CancellationToken cancellationToken)
         {
             _logger.LogInformation("Starting plugin, Downloading Movie Theme Songs...");
-            await _themerrManager.DownloadAllThemerr();
+            await _themerrManager.UpdateAll();
             _logger.LogInformation("All theme songs downloaded");
         }
 
@@ -66,11 +66,11 @@ namespace Jellyfin.Plugin.Themerr.ScheduledTasks
         /// <returns>A list of <see cref="TaskTriggerInfo"/>.</returns>
         public IEnumerable<TaskTriggerInfo> GetDefaultTriggers()
         {
-            // Run this task every 24 hours
+            // Run this task according to the configured interval
             yield return new TaskTriggerInfo
             {
                 Type = TaskTriggerInfo.TriggerInterval,
-                IntervalTicks = TimeSpan.FromHours(24).Ticks
+                IntervalTicks = TimeSpan.FromMinutes(ThemerrPlugin.Instance.Configuration.UpdateInterval).Ticks
             };
         }
     }

Jellyfin.Plugin.Themerr/ThemerrManager.cs
@@ -112,7 +112,7 @@ namespace Jellyfin.Plugin.Themerr
         /// Enumerate through all movies in the library and downloads their theme songs as required.
         /// </summary>
         /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
-        public Task DownloadAllThemerr()
+        public Task UpdateAll()
         {
             var movies = GetMoviesFromLibrary();
             foreach (var movie in movies)

.editorconfig
@@ -32,6 +32,9 @@ dotnet_diagnostic.SA1012.severity = None
 # Closing brace should be preceded by a space
 dotnet_diagnostic.SA1013.severity = None
 
+# Code should not contain trailing whitespace
+dotnet_diagnostic.SA1028.severity = error
+
 # Using directive should appear within a namespace declaration
 dotnet_diagnostic.SA1200.severity = None
 

Jellyfin.Plugin.Themerr.Tests/TestPluginConfiguration.cs
@@ -1,0 +1,43 @@
+﻿using Jellyfin.Plugin.Themerr.Configuration;
+
+namespace Jellyfin.Plugin.Themerr.Tests;
+
+/// <summary>
+/// This class is responsible for testing the <see cref="PluginConfiguration"/>.
+/// </summary>
+[Collection("Fixture Collection")]
+public class TestPluginConfiguration
+{
+    private readonly PluginConfiguration _configuration;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TestPluginConfiguration"/> class.
+    /// </summary>
+    /// <param name="output">An <see cref="ITestOutputHelper"/> instance.</param>
+    public TestPluginConfiguration(ITestOutputHelper output)
+    {
+        _configuration = new PluginConfiguration();
+    }
+
+    /// <summary>
+    /// Test getting the default PluginConfiguration.
+    /// </summary>
+    [Fact]
+    public void TestPluginConfigurationInstance()
+    {
+        // ensure UpdateInterval is an int
+        Assert.IsType<int>(_configuration.UpdateInterval);
+
+        // set UpdateInterval to 15
+        _configuration.UpdateInterval = 15;
+        Assert.Equal(15, _configuration.UpdateInterval);
+
+        // set UpdateInterval lower than minimum allowed value of 15
+        _configuration.UpdateInterval = 14;
+        Assert.Equal(15, _configuration.UpdateInterval);
+
+        // set UpdateInterval to 60
+        _configuration.UpdateInterval = 60;
+        Assert.Equal(60, _configuration.UpdateInterval);
+    }
+}

Jellyfin.Plugin.Themerr.Tests/TestThemerrPlugin.cs
@@ -1,0 +1,69 @@
+﻿using MediaBrowser.Common.Configuration;
+using MediaBrowser.Model.Serialization;
+using Moq;
+
+namespace Jellyfin.Plugin.Themerr.Tests;
+
+/// <summary>
+/// This class is responsible for testing the <see cref="ThemerrPlugin"/>.
+/// </summary>
+[Collection("Fixture Collection")]
+public class TestThemerrPlugin
+{
+    private readonly ThemerrPlugin _plugin;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TestThemerrPlugin"/> class.
+    /// </summary>
+    /// <param name="output">An <see cref="ITestOutputHelper"/> instance.</param>
+    public TestThemerrPlugin(ITestOutputHelper output)
+    {
+        var applicationPaths = new Mock<IApplicationPaths>();
+        applicationPaths.Setup(x => x.PluginsPath).Returns("Plugins");
+
+        var xmlSerializer = new Mock<IXmlSerializer>();
+        _plugin = new ThemerrPlugin(applicationPaths.Object, xmlSerializer.Object);
+    }
+
+    /// <summary>
+    /// Test getting the plugin name.
+    /// </summary>
+    [Fact]
+    public void TestPluginInstance()
+    {
+        Assert.NotNull(_plugin.Name);
+        Assert.Equal("Themerr", _plugin.Name);
+    }
+
+    /// <summary>
+    /// Test getting the plugin description.
+    /// </summary>
+    [Fact]
+    public void TestPluginDescription()
+    {
+        Assert.NotNull(_plugin.Description);
+        Assert.Equal("Downloads Theme Songs", _plugin.Description);
+    }
+
+    /// <summary>
+    /// Test get the plugin id.
+    /// </summary>
+    [Fact]
+    public void TestPluginId()
+    {
+        Assert.Equal(new Guid("84b59a39-bde4-42f4-adbd-c39882cbb772"), _plugin.Id);
+    }
+
+    /// <summary>
+    /// Test getting the plugin configuration page.
+    /// </summary>
+    [Fact]
+    public void TestPluginConfigurationPage()
+    {
+        var pages = _plugin.GetPages();
+        Assert.NotNull(pages);
+        Assert.Single(pages);
+        Assert.Equal("Themerr", pages.First().Name);
+        Assert.Equal("Jellyfin.Plugin.Themerr.Configuration.configPage.html", pages.First().EmbeddedResourcePath);
+    }
+}

Jellyfin.Plugin.Themerr/Configuration/PluginConfiguration.cs
@@ -7,5 +7,27 @@ namespace Jellyfin.Plugin.Themerr.Configuration
     /// </summary>
     public class PluginConfiguration : BasePluginConfiguration
     {
+        private int _updateInterval;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PluginConfiguration"/> class.
+        /// </summary>
+        public PluginConfiguration()
+        {
+            _updateInterval = 60;
+        }
+
+        /// <summary>
+        /// Gets or sets the time between scheduled updates, in minutes.
+        ///
+        /// Minimum value of 15.
+        /// </summary>
+        public int UpdateInterval
+        {
+            get => _updateInterval;
+
+            // todo - modify the existing scheduled task
+            set => _updateInterval = value < 15 ? 15 : value;
+        }
     }
 }

Jellyfin.Plugin.Themerr/Configuration/configPage.html
@@ -1,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Themerr</title>
+</head>
+<body>
+    <div id="ThemerrConfigPage" data-role="page" class="page type-interior pluginConfigurationPage"
+            data-require="emby-input,emby-button,emby-select,emby-checkbox">
+        <div data-role="content">
+            <div class="content-primary">
+                <form class="tbsConfigurationPage">
+                    <div class="sectionTitleContainer flex align-items-center">
+                        <h2 class="sectionTitle">Themerr</h2>
+                        <a is="emby-linkbutton" class="raised button-alt headerHelpButton emby-button" target="_blank"
+                           href="https://app.lizardbyte.dev/support">Support</a>
+                    </div>
+                    <div class="verticalSection">
+                        <p>This plugin relies on the TMDB provider.
+                            Please make sure it is enabled!</p>
+                        <br/>
+                    </div>
+                </form>
+                
+                <form id="ThemerrConfigForm">
+                    <div class="inputContainer">
+                        <label class="inputLabel inputLabelUnfocused" for="UpdateInterval">
+                            Update Interval
+                        </label>
+                        <input id="UpdateInterval" name="UpdateInterval"
+                               type="number" is="emby-input" min="15" />
+                        <div class="fieldDescription">
+                            The interval, in minutes, between theme song updates.
+                        </div>
+                    </div>
+                    <div>
+                        <button is="emby-button" type="submit" class="raised button-submit block emby-button">
+                            <span>Save</span>
+                        </button>
+                    </div>
+                    <br/>
+                    <div>
+                        <button is="emby-button" type="button" class="raised block" id="refresh-library"
+                                onclick=download()>
+                            <span>Update Theme Songs</span>
+                        </button>
+                    </div>
+                </form>
+                <br/>
+            </div>
+        </div>
+
+        <script type="text/javascript">
+            let ThemerrConfig = {
+                pluginUniqueId: '84b59a39-bde4-42f4-adbd-c39882cbb772'
+            }
+
+            document.querySelector('#ThemerrConfigPage')
+                .addEventListener('pageshow', function() {
+                    Dashboard.showLoadingMsg()
+                    ApiClient.getPluginConfiguration(ThemerrConfig.pluginUniqueId).then(function (config) {
+                        document.querySelector('#UpdateInterval').value = config.UpdateInterval
+                        Dashboard.hideLoadingMsg()
+                    })
+                })
+
+            document.querySelector('#ThemerrConfigForm')
+                .addEventListener('submit', function(e) {
+                    Dashboard.showLoadingMsg()
+                    ApiClient.getPluginConfiguration(ThemerrConfig.pluginUniqueId).then(function (config) {
+                        config.UpdateInterval = document.querySelector('#UpdateInterval').value
+                        ApiClient.updatePluginConfiguration(ThemerrConfig.pluginUniqueId, config).then(function (result) {
+                            Dashboard.processPluginConfigurationUpdateResult(result)
+                        })
+                    })
+
+                    e.preventDefault()
+                    return false
+                })
+            function download() {
+                let request = {
+                    url: ApiClient.getUrl('/Themerr/TriggerUpdate'),
+                    type: 'POST'
+                }
+
+                ApiClient.fetch(request).then(function () {
+                    Dashboard.alert("Updating Theme Songs...")
+                }).catch(function () {
+                    Dashboard.alert({
+                        message: "Unexpected error occurred!"
+                    })
+                })
+            }
+        </script>
+    </div>
+</body>
+</html>

commit 452a9ef3a9957925fb50633fafaeb2e919831402

fix: remove BasePlugin inheritance from ThemerrManager
ThemerrManager and ThemerrPlugin both extended BasePlugin<PluginConfiguration>,
giving the assembly two IPlugin implementations. Jellyfin's loader picked one
non-deterministically; when it chose ThemerrManager (which has no IHasWebPages),
the config page URL returned 404. This bug was introduced in #189 when
ThemerrPlugin was split out but ThemerrManager was never cleaned up.

ThemerrManager never used Configuration, ApplicationPaths, or XmlSerializer
from its BasePlugin base — those parameters only existed to satisfy the base
constructor call and are now removed throughout the call chain.

Co-Authored-By: Claude Sonnet 4.6 <noreply@anthropic.com>

Jellyfin.Plugin.Themerr.Tests/TestThemerrController.cs
@@ -1,9 +1,7 @@
 ﻿using System.Collections;
 using Jellyfin.Plugin.Themerr.Api;
-using MediaBrowser.Common.Configuration;
 using MediaBrowser.Controller.Configuration;
 using MediaBrowser.Controller.Library;
-using MediaBrowser.Model.Serialization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moq;
@@ -26,12 +24,10 @@ public class TestThemerrController
     {
         TestLogger.Initialize(output);
 
-        Mock<IApplicationPaths> mockApplicationPaths = TestHelper.GetMockApplicationPaths();
         Mock<ILibraryManager> mockLibraryManager = new();
         Mock<ILogger<ThemerrController>> mockLogger = new();
         Mock<IServerConfigurationManager> mockServerConfigurationManager = new();
         Mock<ILoggerFactory> mockLoggerFactory = new();
-        Mock<IXmlSerializer> mockXmlSerializer = new();
 
         // Create a TestableServerConfiguration with UICulture set to "en-US"
         var testableServerConfiguration = new TestableServerConfiguration("en-US");
@@ -40,12 +36,10 @@ public class TestThemerrController
         mockServerConfigurationManager.Setup(x => x.Configuration).Returns(testableServerConfiguration);
 
         _controller = new ThemerrController(
-            mockApplicationPaths.Object,
             mockLibraryManager.Object,
             mockLogger.Object,
             mockServerConfigurationManager.Object,
-            mockLoggerFactory.Object,
-            mockXmlSerializer.Object);
+            mockLoggerFactory.Object);
     }
 
     /// <summary>

Jellyfin.Plugin.Themerr.Tests/TestThemerrManager.cs
@@ -1,9 +1,7 @@
-using MediaBrowser.Common.Configuration;
 using MediaBrowser.Controller.Entities;
 using MediaBrowser.Controller.Entities.Movies;
 using MediaBrowser.Controller.Entities.TV;
 using MediaBrowser.Controller.Library;
-using MediaBrowser.Model.Serialization;
 using Microsoft.Extensions.Logging;
 using Moq;
 using Newtonsoft.Json;
@@ -29,10 +27,8 @@ public class TestThemerrManager
     {
         TestLogger.Initialize(output);
 
-        Mock<IApplicationPaths> mockApplicationPaths = TestHelper.GetMockApplicationPaths();
         Mock<ILibraryManager> mockLibraryManager = new();
         Mock<ILogger<ThemerrManager>> mockLogger = new();
-        Mock<IXmlSerializer> mockXmlSerializer = new();
 
         var audioStubPath = Path.Combine(Directory.GetCurrentDirectory(), "data", "audio_stub.mp3");
 
@@ -46,16 +42,12 @@ public class TestThemerrManager
             });
 
         _themerrManager = new ThemerrManager(
-            mockApplicationPaths.Object,
             mockLibraryManager.Object,
-            mockLogger.Object,
-            mockXmlSerializer.Object);
+            mockLogger.Object);
 
         _themerrManagerWithMockYoutube = new ThemerrManager(
-            mockApplicationPaths.Object,
             mockLibraryManager.Object,
             mockLogger.Object,
-            mockXmlSerializer.Object,
             mockYoutubeClient.Object);
     }
 

Jellyfin.Plugin.Themerr/Api/ThemerrController.cs
@@ -7,10 +7,8 @@ using System.Net.Mime;
 using System.Reflection;
 using System.Threading.Tasks;
 using MediaBrowser.Common.Api;
-using MediaBrowser.Common.Configuration;
 using MediaBrowser.Controller.Configuration;
 using MediaBrowser.Controller.Library;
-using MediaBrowser.Model.Serialization;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -36,25 +34,19 @@ namespace Jellyfin.Plugin.Themerr.Api
         /// <summary>
         /// Initializes a new instance of the <see cref="ThemerrController"/> class.
         /// </summary>
-        /// <param name="applicationPaths">The application paths.</param>
         /// <param name="libraryManager">The library manager.</param>
         /// <param name="logger">The logger.</param>
         /// <param name="configurationManager">The configuration manager.</param>
         /// <param name="loggerFactory">The logger factory.</param>
-        /// <param name="xmlSerializer">The XML serializer.</param>
         public ThemerrController(
-            IApplicationPaths applicationPaths,
             ILibraryManager libraryManager,
             ILogger<ThemerrController> logger,
             IServerConfigurationManager configurationManager,
-            ILoggerFactory loggerFactory,
-            IXmlSerializer xmlSerializer)
+            ILoggerFactory loggerFactory)
         {
             _themerrManager = new ThemerrManager(
-                applicationPaths,
                 libraryManager,
-                loggerFactory.CreateLogger<ThemerrManager>(),
-                xmlSerializer);
+                loggerFactory.CreateLogger<ThemerrManager>());
             _logger = logger;
             _configurationManager = configurationManager;
         }

Jellyfin.Plugin.Themerr/ScheduledTasks/ThemerrTasks.cs
@@ -2,9 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using MediaBrowser.Common.Configuration;
 using MediaBrowser.Controller.Library;
-using MediaBrowser.Model.Serialization;
 using MediaBrowser.Model.Tasks;
 using Microsoft.Extensions.Logging;
 
@@ -21,24 +19,18 @@ namespace Jellyfin.Plugin.Themerr.ScheduledTasks
         /// <summary>
         /// Initializes a new instance of the <see cref="ThemerrTasks"/> class.
         /// </summary>
-        /// <param name="applicationPaths">The application paths.</param>
         /// <param name="libraryManager">The library manager.</param>
         /// <param name="logger">The logger.</param>
         /// <param name="loggerFactory">The logger factory.</param>
-        /// <param name="xmlSerializer">The XML serializer.</param>
         public ThemerrTasks(
-            IApplicationPaths applicationPaths,
             ILibraryManager libraryManager,
             ILogger<ThemerrTasks> logger,
-            ILoggerFactory loggerFactory,
-            IXmlSerializer xmlSerializer)
+            ILoggerFactory loggerFactory)
         {
             _logger = logger;
             _themerrManager = new ThemerrManager(
-                applicationPaths,
                 libraryManager,
-                loggerFactory.CreateLogger<ThemerrManager>(),
-                xmlSerializer);
+                loggerFactory.CreateLogger<ThemerrManager>());
         }
 
         /// <summary>

Jellyfin.Plugin.Themerr/ThemerrManager.cs
@@ -6,15 +6,11 @@ using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Jellyfin.Data.Enums;
-using Jellyfin.Plugin.Themerr.Configuration;
-using MediaBrowser.Common.Configuration;
-using MediaBrowser.Common.Plugins;
 using MediaBrowser.Controller.Entities;
 using MediaBrowser.Controller.Entities.Movies;
 using MediaBrowser.Controller.Entities.TV;
 using MediaBrowser.Controller.Library;
 using MediaBrowser.Model.Entities;
-using MediaBrowser.Model.Serialization;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
@@ -23,7 +19,7 @@ namespace Jellyfin.Plugin.Themerr
     /// <summary>
     /// The main entry point for the plugin.
     /// </summary>
-    public class ThemerrManager : BasePlugin<PluginConfiguration>, IDisposable
+    public class ThemerrManager : IDisposable
     {
         private readonly ILibraryManager _libraryManager;
         private readonly Timer _timer;
@@ -33,29 +29,19 @@ namespace Jellyfin.Plugin.Themerr
         /// <summary>
         /// Initializes a new instance of the <see cref="ThemerrManager"/> class.
         /// </summary>
-        /// <param name="applicationPaths">The application paths.</param>
         /// <param name="libraryManager">The library manager.</param>
         /// <param name="logger">The logger.</param>
-        /// <param name="xmlSerializer">The XML serializer.</param>
         /// <param name="youtubeClientWrapper">The YouTube client wrapper. Uses the default implementation when null.</param>
         public ThemerrManager(
-            IApplicationPaths applicationPaths,
             ILibraryManager libraryManager,
             ILogger<ThemerrManager> logger,
-            IXmlSerializer xmlSerializer,
             IYoutubeClientWrapper youtubeClientWrapper = null)
-            : base(applicationPaths, xmlSerializer)
         {
             _libraryManager = libraryManager;
             _logger = logger;
             _timer = new Timer(_ => OnTimerElapsed(), null, Timeout.Infinite, Timeout.Infinite);
             _youtubeClientWrapper = youtubeClientWrapper ?? new YoutubeClientWrapper();
         }
-
-        /// <summary>
-        /// Gets the plugin instance.
-        /// </summary>
-        public override string Name => "Themerr";
 
         /// <summary>
         /// Get a value from the themerr data file if it exists.