2c8c725211be827734a7ef08e9fcb4aafbe7da53
ReenigneArcher <42013603+ReenigneArcher@users.noreply.github.com>
2026-09-18 20:50:17 -0400

    fix: refine Dockle documentation

--- docs/source/dockle_conf.py
+++ docs/source/dockle_conf.py
@@ -96,8 +96,6 @@ extensions.extend([
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['toc.rst']
-
 # Extensions to include.
 source_suffix = {
     '.rst': 'restructuredtext',
